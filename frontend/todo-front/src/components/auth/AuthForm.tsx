fc1c5aa3c19c734c900fd2a1e9d332c04a73fe28
nakamoto kousuke <kosuke.nakamoto@g.dreamcareer.co.jp>
2024-11-11 09:45:17 +0900

    useForm

--- frontend/todo-front/src/components/auth/AuthForm.tsx
+++ frontend/todo-front/src/components/auth/AuthForm.tsx
@@ -1,57 +1,50 @@
-import React, { useState } from "react";
 import { Button } from "../ui/button";
+import { useForm } from "react-hook-form";
+import { formData } from "@/types";
 
 interface AuthFormProps {
-  onSubmit: (email: string, password: string) => Promise<void>;
+  onSubmit: (formData: formData) => Promise<void>;
   buttonText: string;
 }
 
 export const AuthForm = ({ onSubmit, buttonText }: AuthFormProps) => {
-  const [email, setEmail] = useState("sample@gmail.com");
-  const [password, setPassword] = useState("123456789");
-  const [error, setError] = useState("");
+  // const [email, setEmail] = useState("sample@gmail.com");
+  // const [password, setPassword] = useState("123456789");
+  // const [error, setError] = useState("");
+  const { register, handleSubmit } = useForm<{
+    email: string;
+    password: string;
+  }>();
 
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-    try {
-      await onSubmit(email, password); // 親コンポーネントに渡されたonSubmitを呼ぶ
-    } catch (error) {
-      console.log(error);
+  // const handleSubmit = async (event: React.FormEvent) => {
+  //   event.preventDefault();
+  //   try {
+  //     await onSubmit(email, password); // 親コンポーネントに渡されたonSubmitを呼ぶ
+  //   } catch (error) {
+  //     console.log(error);
 
-      setError("サーバーとの通信に失敗しました。");
-    }
-  };
+  //     setError("サーバーとの通信に失敗しました。");
+  //   }
+  // };
 
   return (
     <div className="p-8 rounded-md shadow-md ">
-      {error && <p style={{ color: "red" }}>{error}</p>}
+      {/* {error && <p style={{ color: "red" }}>{error}</p>} */}
       <form
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
         className="space-y-4 flex flex-col items-end"
       >
         <div>
           <div>
             <label>Email</label>
           </div>
-          <input
-            className="border rounded-md"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+          <input className="border rounded-md" {...register("email")} />
         </div>
         <div>
           <div>
             <label>Password</label>
           </div>
-          <input
-            className="border rounded-md"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+          <input className="border rounded-md" {...register("password")} />
         </div>
         <Button type="submit" variant={"login"}>
           {buttonText}
--- frontend/todo-front/src/types/index.ts
+++ frontend/todo-front/src/types/index.ts
@@ -1,17 +1,4 @@
-// export type Todo = {
-//   id: number;
-//   title: string;
-//   detail: string | null;
-//   completed: boolean;
-//   createdAt: Date;
-//   updatedAt: Date;
-//   dueDate: Date | null;
-//   userId: number;
-// };
-
-// export type User = {
-//   id: number;
-//   email: string;
-//   password: string;
-//   createdAt: Date;
-// };
+export type formData = {
+  email: string;
+  password: string;
+};
