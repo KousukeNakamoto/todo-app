8a1611861dc016e655999381d9055e7dac386469
nakamoto kousuke <kosuke.nakamoto@g.dreamcareer.co.jp>
2024-11-14 12:30:03 +0900

    ログイン画面にuseFormとzodを適応

--- frontend/todo-front/src/components/auth/AuthForm.tsx
+++ frontend/todo-front/src/components/auth/AuthForm.tsx
@@ -1,37 +1,44 @@
 import { Button } from "../ui/button";
 import { useForm } from "react-hook-form";
 import { formData } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { useState } from "react";
 
 interface AuthFormProps {
   onSubmit: (formData: formData) => Promise<void>;
   buttonText: string;
 }
 
+const schema = z.object({
+  email: z.string().email("正しいメールアドレスを入力してください"),
+  password: z.string().min(3),
+});
+
+type FormValue = z.infer<typeof schema>;
+
 export const AuthForm = ({ onSubmit, buttonText }: AuthFormProps) => {
-  // const [email, setEmail] = useState("sample@gmail.com");
-  // const [password, setPassword] = useState("123456789");
-  // const [error, setError] = useState("");
-  const { register, handleSubmit } = useForm<{
-    email: string;
-    password: string;
-  }>();
+  const [error, setError] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isLoading },
+  } = useForm<FormValue>({ resolver: zodResolver(schema), mode: "onBlur" });
 
-  // const handleSubmit = async (event: React.FormEvent) => {
-  //   event.preventDefault();
-  //   try {
-  //     await onSubmit(email, password); // 親コンポーネントに渡されたonSubmitを呼ぶ
-  //   } catch (error) {
-  //     console.log(error);
-
-  //     setError("サーバーとの通信に失敗しました。");
-  //   }
-  // };
+  const errorHandle = async ({ email, password }: FormValue) => {
+    try {
+      setError(false);
+      await onSubmit({ email, password }); // 親コンポーネントに渡されたonSubmitを呼ぶ
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
+  };
 
   return (
     <div className="p-8 rounded-md shadow-md ">
-      {/* {error && <p style={{ color: "red" }}>{error}</p>} */}
       <form
-        onSubmit={handleSubmit(onSubmit)}
+        onSubmit={handleSubmit(errorHandle)}
         className="space-y-4 flex flex-col items-end"
       >
         <div>
@@ -39,16 +46,23 @@ export const AuthForm = ({ onSubmit, buttonText }: AuthFormProps) => {
             <label>Email</label>
           </div>
           <input className="border rounded-md" {...register("email")} />
+          {errors.email && (
+            <p className="text-red-600">{errors.email.message}</p>
+          )}
         </div>
         <div>
           <div>
             <label>Password</label>
           </div>
           <input className="border rounded-md" {...register("password")} />
+          {errors.password && (
+            <p className="text-red-600">{errors.password.message}</p>
+          )}
         </div>
-        <Button type="submit" variant={"login"}>
+        <Button type="submit" variant={"login"} disabled={isLoading}>
           {buttonText}
         </Button>
+        {error && <p className="text-red-600">error</p>}
       </form>
     </div>
   );
